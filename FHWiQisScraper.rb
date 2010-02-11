6d25a2db04b4db507a94e8358e33139e18dcb450
Robert <robert.bahmann@gmail.com>
2010-02-11 14:24:41 +0100

    angepasst an die neue struktur der HS-RM seite

--- FHWiQisScraper.rb
+++ FHWiQisScraper.rb
@@ -8,11 +8,11 @@ class FHWiQisScraper < QisScraper
   attr_reader :modules
   def initialize(name,password)
     super(name,password)
-    @curriculum_body = agent.get('http://web-1k.rz.fh-wiesbaden.de/bachelorcurriculum.cfm?fb=22&sprachid=1&sid=103&poid=93&detail=ja').body
+    @curriculum_body = agent.get('http://www.hs-rm.de/dcsm/studiengaenge/allgemeine-informatik-bsc/studienverlauf-curriculum/index.html').body
     @modules = fetchModules
     fetchCreditPoints
   end
-  
+
   def getAverageGrade
     puts "Calculating average grade"
     allCp = 0;
@@ -24,7 +24,7 @@ class FHWiQisScraper < QisScraper
     }
     return gradeCp/allCp
   end
-  
+
   private
   def fetchCreditPoints
     puts "Fetching Creditpoints"
@@ -33,18 +33,23 @@ class FHWiQisScraper < QisScraper
       fetchCPforModul(modul)
     }
   end
-  
+
   def fetchCPforModul(modul)
     # >1000 because of the mysterios modules without any sense
     if modul.isMainModul and modul.modulNr > 1000 then
-    doc = Hpricot(@curriculum_body)
-    table = doc.at("//table[1]")
-    rows = (table/"tr")
-    selRow = rows.select{|row| row.innerText.include?(modul.modulNr.to_s)}
-    cells = (selRow[0]/"td")
-    modul.cp =  cells[3].innerText.strip.to_f
+      doc = Hpricot(@curriculum_body)
+      table = doc.at("/html/body/div/div[3]/div[2]/div[2]/div/table/tbody")
+      rows = (table/"tr")
+      selRow = rows.select{|row| row.innerText.include?(modul.modulNr.to_s)}
+      if selRow[0].nil?
+        puts "Unable to fetch CP for #{modul.modulText}"
+        modul.cp = 0.0
+      else
+        cells = (selRow[0]/"td")
+        modul.cp =  cells[2].innerText.strip.to_f
+      end
     else
-    modul.cp = 0.0
+      modul.cp = 0.0
     end
   end
 end
